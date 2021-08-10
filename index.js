65444d4f683b6d5ed6fb6dbf96dbcda7dcdd2e7b
Sander Iwase <sanderiw@gmail.com>
2021-08-10 15:52:25 -0300

    complete fizzbuzz minilab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Seu código aqui!
+// Fizzbuzz Minilab
+const initNumber = 1; //First printed number
+const finalNumber = 100; //Final printed number
+
+//Analyze each number
+for (let i=initNumber; i<=finalNumber; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('Fizz Buzz'); //Multiple of 3 and 5 at the same time
+    } else if (i % 3 === 0) {
+        console.log('Fizz');  //Multiple of 3
+    } else if (i % 5 === 0) {
+        console.log('Buzz');  //Multiple of 5
+    } else {
+        console.log(i);  //Else, print the number itself
+    }
+}
